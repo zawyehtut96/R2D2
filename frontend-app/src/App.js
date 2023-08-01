831cf1fd2417bd80ac037dd9a05d0b3803b4b6ff
zackjupyter <zaw.ye.htut1996@gmail.com>
2023-08-01 16:06:15 -0500

    Initial commit

---
+++ frontend-app/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import CompetitorResearch from "./CompetitorResearch";
+import PersonalizedEmail from "./PersonalizedEmail";
+import SocialMediaPost from "./SocialMediaPost";
+
+const App = () => {
+  return (
+    <div>
+      <CompetitorResearch />
+      <PersonalizedEmail />
+      <SocialMediaPost />
+    </div>
+  );
+};
+
+export default App;
